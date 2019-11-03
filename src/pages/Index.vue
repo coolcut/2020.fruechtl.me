e69aaeda6231839173772d1de5bbc2dcff04bf36
Jan Früchtl <jan@fruechtl.me>
2019-11-03 21:10:14 +0100

    Basic structure for first post

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,17 @@
 <template>
-  <Layout></Layout>
+  <Layout>
+    <h2>Hi there!</h2>
+    <p>
+      A week ago, I saw
+      <a
+        href="https://twitter.com/destroytoday/status/1188617015583526913"
+      >this tweet</a> by
+      <a href="https://twitter.com/destroytoday">Jonnie Hallman</a>, which inspired me to revamp my personal website again. As Jonnie wrote in his initial post, he wants to capture his progress on this new personal website in a journal as he goes. I loved this idea and want to try the same thing, but maybe a bit different, we will see.
+    </p>
+    <p>From where I see it now, my new website should be more focused on me, a bit more personal stuff, and a bit less "professional/portfolio" style. It should be more tailored towards me and what I want to experiment with in the future.</p>
+    <p>For the first steps, I'm giving Gridsome a try. Gridsome is not my final decision yet, and I want to see if it can help me with the goals that currently occupy my head and also deepen my Vue knowledge even further.</p>
+    <p>Maybe this way of writing will finally break my struggles in journaling or keeping a blog up-to-date.</p>
+  </Layout>
 </template>
 
 <script>
