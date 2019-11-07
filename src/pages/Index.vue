6b8cee3fae77bca7d7d0a14bd39e30ca666ba587
Jan Früchtl <jan@fruechtl.me>
2019-11-07 21:31:25 +0100

    Basic date formatting

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@
     <section>
       <article v-for="{ node } in $page.posts.edges" :key="node._id">
         <h2>{{node.title}}</h2>
+        <time :datetime="node.publishDate">{{humanReadableDate(node.publishDate)}}</time>
         <div v-html="markdownToHTML(node.body)"></div>
       </article>
     </section>
@@ -26,6 +27,7 @@ query {
 
 <script>
 var MarkdownIt = require("markdown-it")();
+import moment from "moment";
 
 export default {
   metaInfo: {
@@ -35,6 +37,10 @@ export default {
   methods: {
     markdownToHTML(content) {
       return MarkdownIt.render(content);
+    },
+
+    humanReadableDate(date) {
+      return moment(date).format("dddd, MMMM Do YYYY");
     }
   }
 };
