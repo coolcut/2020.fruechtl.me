3b6fb68716f57bc8210790421c3b00af09dd67d7
Jan Früchtl <jan@fruechtl.me>
2020-06-28 15:25:27 +0200

    Fix likes

--- _data/likes.js
+++ _data/likes.js
@@ -1,14 +1,27 @@
-const Cache = require("@11ty/eleventy-cache-assets");
+const { RemoteAssetCache } = require('@11ty/eleventy-cache-assets');;
 require('dotenv').config();
 
 const USERNAME = process.env.PINBOARD_USERNAME;
 const PASSWORD = process.env.PINBOARD_PASSWORD;
 
-module.exports = async function() {
-  let json = await Cache(`https://${USERNAME}:${PASSWORD}@api.pinboard.in/v1/posts/all?format=json&tag=like`, {
-    duration: "1d",
-    type: "json"
-  });
+class PinboardAssetCache extends RemoteAssetCache {
+  async getResponseValue(response, type) {
+    if(type === "json") {
+      let text = await response.text();
+      return JSON.parse(text.trim());
+    } else if(type === "text") {
+      return response.text();
+    }
+    return response.buffer();
+  }
+}
 
-  return json;
-};
+async function PinboardCache(url, options) {
+  let asset = new PinboardAssetCache(url, options.directory);
+  return asset.fetch(options);
+}
+
+module.exports = PinboardCache(`https://${USERNAME}:${PASSWORD}@api.pinboard.in/v1/posts/all?format=json&tag=like`, {
+   duration: '1h',
+   type: 'json',
+ });
